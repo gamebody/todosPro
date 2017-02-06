8d1a579c834135a394bd4eb0b4578ff0f60f02bc
liuyun <atme_talk@163.com>
2017-02-06 09:53:04 +0800

    add stylus-loader

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -32,10 +32,9 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
+<style lang="stylus" scoped>
+h1, h2
+  font-weight: normal
 
 ul {
   list-style-type: none;
