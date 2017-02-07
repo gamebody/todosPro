eda1c3dd34d0ad5ce3f583ed264ec7948ebc91cd
liuyun <atme_talk@163.com>
2017-02-07 11:45:40 +0800

    使用rem来适配移动端

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import { adapt } from 'src/tool/initRem'
+
+// 设置设计稿的尺寸为750，1rem = 100px
+const designWidth = 750
+const rem2px = 100
+adapt(designWidth, rem2px)
 
 /* eslint-disable no-new */
 new Vue({
