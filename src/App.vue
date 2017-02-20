d49240ec332a16d2937670fe9bd7f451d0350e0d
liuyun <atme_talk@163.com>
2017-02-20 21:19:11 +0800

    transition

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <transition name="view-left" mode="">
+      <router-view></router-view>  
+    </transition>
   </div>
 </template>
 
@@ -15,5 +17,14 @@ export default {
     width: 100%
     height: 100%
     font-family: 'Avenir', Helvetica, Arial, sans-serif
-
+    .view-left-enter
+      transform: translate(100%)
+    .view-left-enter-to, .view-left-leave
+      transform: translate(0)
+    .view-left-enter-active
+    .view-left-leave-active
+      transition: all .5s ease
+    .view-left-leave-to
+      transform: translate(-100%)
+      
 </style>
